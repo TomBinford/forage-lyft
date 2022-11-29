056d5e900873210881b322ecb6b1dde253c3c6f3
Tom Binford <tjbof123@gmail.com>
2022-11-28 20:41:21 -0800

    Make engine constructor parameter order consistent with that of battery

--- engine/capulet_engine.py
+++ engine/capulet_engine.py
@@ -2,9 +2,9 @@ from engine import Engine
 
 
 class CapuletEngine(Engine):
-    def __init__(self, current_mileage: int, last_service_mileage: int):
-        self.current_mileage = current_mileage
+    def __init__(self, last_service_mileage: int, current_mileage: int):
         self.last_service_mileage = last_service_mileage
+        self.current_mileage = current_mileage
 
     def should_be_serviced(self):
         return self.current_mileage - self.last_service_mileage > 30000
