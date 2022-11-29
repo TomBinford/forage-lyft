aead99c4dc45adf3b6621a26a64189f3cd6916b2
Tom Binford <tjbof123@gmail.com>
2022-11-28 20:38:46 -0800

    Add type hints to engine classes

--- engine/sternman_engine.py
+++ engine/sternman_engine.py
@@ -2,7 +2,7 @@ from engine import Engine
 
 
 class SternmanEngine(Engine):
-    def __init__(self, warning_light_is_on):
+    def __init__(self, warning_light_is_on: bool):
         self.warning_light_is_on = warning_light_is_on
 
     def should_be_serviced(self):
