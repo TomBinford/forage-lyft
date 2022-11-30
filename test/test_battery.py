4ab44f2676feaab1559d4ddaef4812fe117326f9
Tom Binford <tjbof123@gmail.com>
2022-11-29 16:49:31 -0800

    Use timedelta instead of datetime.replace for creating a time in the future. This avoids issues with days near the end of the month where the adjusted day would be invalid.

--- test/test_battery.py
+++ test/test_battery.py
@@ -1,31 +1,31 @@
 import unittest
-from datetime import datetime
+from datetime import datetime, timedelta
 from battery import *
 
 
 class TestBattery(unittest.TestCase):
     def test_spindler_should_be_serviced(self):
         today = datetime.today().date()
-        future = today.replace(year=today.year + 2, day=today.day + 1)
+        future = today + timedelta(days=365 * 2 + 1)
         spindler = SpindlerBattery(today, future)
         print(spindler.should_be_serviced())
         self.assertTrue(spindler.should_be_serviced())
 
     def test_spindler_should_not_be_serviced(self):
         today = datetime.today().date()
-        future = today.replace(year=today.year + 1)
+        future = today + timedelta(days=365)
         spindler = SpindlerBattery(today, future)
         self.assertFalse(spindler.should_be_serviced())
 
     def test_nubbin_should_be_serviced(self):
         today = datetime.today().date()
-        future = today.replace(year=today.year + 4, day=today.day + 1)
+        future = today + timedelta(days=365 * 4 + 1)
         nubbin = NubbinBattery(today, future)
         self.assertTrue(nubbin.should_be_serviced())
 
     def test_nubbin_should_not_be_serviced(self):
         today = datetime.today().date()
-        future = today.replace(year=today.year + 3)
+        future = today + timedelta(days=365 * 3)
         nubbin = NubbinBattery(today, future)
         self.assertFalse(nubbin.should_be_serviced())
 
